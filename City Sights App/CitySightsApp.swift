bfbe44b9b121fda76bea3e3e775ee02c1d1098c4
Pouria Tolouei <pouria81.t@gmail.com>
2022-08-10 11:32:08 +0800

    - Add CoreLocation to get user permission and their current location - Create the ContentModel file and ContentModel observable object - Add the locationManager property to ContentModel - Add a key to info.plist to show location permission menu and message - Add the necessary components to set ContentModel as delegate of locationManager - Add delegate method to keep track of the authorisation status and respond accordingly - Add delegate method to get the user's location

--- City Sights App/CitySightsApp.swift
+++ City Sights App/CitySightsApp.swift
@@ -1,5 +1,5 @@
 //
-//  City_Sights_App.swift
+//  CitySightsApp.swift
 //  City Sights App
 //
 //  Created by Pouria Tolouei on 09/08/2022.
@@ -8,10 +8,11 @@
 import SwiftUI
 
 @main
-struct City_Sights_App: App {
+struct CitySightsApp: App {
     var body: some Scene {
         WindowGroup {
             LaunchView()
+                .environmentObject(ContentModel())
         }
     }
 }
