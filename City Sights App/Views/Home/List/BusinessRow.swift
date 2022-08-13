6dafbf2074ee873f5184e76f5548e5bfb347be20
Pouria Tolouei <pouria81.t@gmail.com>
2022-08-13 13:34:17 +0800

    - Build the BusinessRow - Call the BusinessRow view in Business Section - Map custom property names in Business model to the json keys - Add a property in Business model to store image data - Add a method in Business model to get image data from the image url - Make the Business model a class and an observable object in order to populate the image data later on - Call the image data method in ContentModel to populate the image data property after parsing - Sort the businesses array in ContentModel in ascending order of distance - Add padding to the sides and the top in HomeView - Hide the scrollView bar in BusinessList

---
+++ City Sights App/Views/Home/List/BusinessRow.swift
@@ -0,0 +1,49 @@
+//
+//  BusinessRow.swift
+//  City Sights App
+//
+//  Created by Pouria Tolouei on 12/08/2022.
+//
+
+import SwiftUI
+
+struct BusinessRow: View {
+    
+    @ObservedObject var business: Business
+    
+    var body: some View {
+        
+        VStack(alignment: .leading) {
+            
+            HStack {
+                
+                // Image
+                let uiImage = UIImage(data: business.imageData ?? Data())
+                Image(uiImage: uiImage ?? UIImage())
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 58, height: 58)
+                    .cornerRadius(5)
+                
+                // Name and distance
+                VStack(alignment: .leading) {
+                    Text(business.name ?? "")
+                        .bold()
+                    Text(String(format:"%.1f km away", (business.distance ?? 0)/1000))
+                        .font(.caption)
+                }
+                
+                Spacer()
+                
+                // Star rating and number of reviews
+                VStack(alignment: .leading) {
+                    Image("regular_\(business.rating ?? 0.0)")
+                    Text("\(business.reviewCount ?? 0) Reviews")
+                        .font(.caption)
+                }
+            }
+            
+            Divider()
+        }
+    }
+}
