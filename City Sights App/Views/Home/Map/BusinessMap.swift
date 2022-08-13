bb93ca26a93652c9a47e4a2545f698feafeae7b2
Pouria Tolouei <pouria81.t@gmail.com>
2022-08-13 21:51:21 +0800

    - Add a coordinator/delegate to the BusinessMap view - Implement a coordinator function to create/reuse an annotationView for each annotation

--- City Sights App/Views/Home/Map/BusinessMap.swift
+++ City Sights App/Views/Home/Map/BusinessMap.swift
@@ -38,6 +38,7 @@ struct BusinessMap: UIViewRepresentable {
     func makeUIView(context: Context) -> MKMapView {
         
         let mapView = MKMapView()
+        mapView.delegate = context.coordinator
         
         // Make the user show up on the map
         mapView.showsUserLocation = true
@@ -50,8 +51,8 @@ struct BusinessMap: UIViewRepresentable {
     
     func updateUIView(_ uiView: MKMapView, context: Context) {
         
-//        // Remove all annotations
-//        uiView.removeAnnotations(uiView.annotations)
+        // Remove all annotations
+        uiView.removeAnnotations(uiView.annotations)
         
         // Add the ones based on the business
         uiView.showAnnotations(self.locations, animated: true)
@@ -60,5 +61,41 @@ struct BusinessMap: UIViewRepresentable {
     static func dismantleUIView(_ uiView: MKMapView, coordinator: ()) {
         
         uiView.removeAnnotations(uiView.annotations)
+    }
+    
+    // MARK - Coordinator class
+    func makeCoordinator() -> Coordinator {
+        return Coordinator()
+    }
+    
+    class Coordinator: NSObject, MKMapViewDelegate {
+        
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            
+            // If the annotation is the user blue dot, return nil
+            if annotation is MKUserLocation {
+                return nil
+            }
+            
+            // Check if there's a reusable annotation view first
+            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: Constants.annotationReuseId)
+            
+            if annotationView == nil {
+                
+                // Create a new one if there's no reusable one
+                annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: Constants.annotationReuseId)
+                
+                annotationView!.canShowCallout = true
+                annotationView!.rightCalloutAccessoryView  = UIButton(type: .detailDisclosure)
+            }
+            else {
+                
+                // Got a reusable one
+                annotationView!.annotation = annotation
+            }
+            
+            // Return it
+            return annotationView
+        }
     }
 }
